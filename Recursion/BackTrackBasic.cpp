a992be6675a2ea28cfa0574567df24d1379b992f
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-11-12 06:52:54 +0600

    --

--- Recursion/BackTrackBasic.cpp
+++ Recursion/BackTrackBasic.cpp
@@ -1,8 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-void print(int n){
-    
+void print(int n, int i){
+    if(i<1) return;
+    print(n,i-1);
+    cout << i << " ";
 }
 int main(){
+    int n;
+    cin >> n;
+    print(n,n);
     return 0;
 }
