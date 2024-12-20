86674acb6d82338c739b443c0be10bca2d809c7a
mrakib007 <mrakibhasan199744@gmail.com>
2024-12-20 14:36:12 +0600

    Two pointers approach

--- Problems/Array/Medium/2sumProblem.cpp
+++ Problems/Array/Medium/2sumProblem.cpp
@@ -1,10 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Two Pointers Approach
+string twoSumTwoPointer(vector<int>& nums,int target){
+    int n=nums.size();
+    int left=0,right=n-1;
+    sort(nums.begin(),nums.end());
+    while(right>left){
+        int sum=nums[left]+nums[right];
+        if(sum==target){
+            return "YES";
+        }else if(sum<target){
+            right--;
+        }else{
+            left++;
+        }
+    }
+    return "NO";
+}
+//Better And Optimal Approach
+vector<int>twoSumBetterApproach(vector<int>& nums,int target){
+    int n=nums.size();
+    map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        int num=nums[i];
+        int rem=target-num;
+        if(mpp.find(rem)!=mpp.end()){
+            return {mpp[rem],i};
+        }
+        mpp[num]=i;
+    }
+    return {};
+}
 //Brute Force Approach
 vector<int>twoSum(vector<int>& nums,int target){
     int n=nums.size();
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
+            if(i=j) continue;
             if(nums[i]+nums[j]==target){
                 return {i,j};
             }
@@ -27,3 +59,5 @@ int main(){
     return 0;
 }
 //For Brute Force the time complexity is O(n^2) and space complexity is O(1)
+//For Better Approach the time complexity is O(n) and space complexity is O(n)
+//For Two Pointer Approach the time complexity is O(nlogn) and space complexity is O(1)
