65c05357d4662252ed288f3ce05e7b66613ee7cf
mrakib007 <mrakibhasan199744@gmail.com>
2024-12-21 21:47:05 +0600

    Majority element optimized by Moore's algorithm

--- Problems/Array/Medium/majorityElement.cpp
+++ Problems/Array/Medium/majorityElement.cpp
@@ -26,6 +26,32 @@ int majorityElementBetter(vector<int>arr){
     }
     return -1;
 }
+//Optimal Approach using Moore's Algorithm
+//Time complexity: O(n), Space Complexity: O(1)
+int majorityElementOptimal(vector<int>& nums) {
+    int count1=0;
+    int element;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        if(count1==0){
+            element=nums[i];
+            count1++;
+            }else if(nums[i]==element){
+                count1++;
+            }else{
+                count1--;
+            }
+        }
+        int count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==element){
+                count2++;
+            }
+        }
+        if(count2>n/2) return element;
+        return -1;
+    
+}
 int main(){
     int n;
     cin >> n;
