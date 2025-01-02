988f3239c4b25a03a6be44067a12f167b39c5bec
mrakib007 <mrakibhasan199744@gmail.com>
2025-01-02 22:38:45 +0600

    Longest consequtive optimal

--- Problems/Array/Medium/longestSubsequence.cpp
+++ Problems/Array/Medium/longestSubsequence.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimal Approach. This will have O(n) time complexity and O(n) space complexity
+int longestSubsequenceOptimal(vector<int>&arr){
+    int n=arr.size();
+    if(n==0) return 0;
+    unordered_set<int>st;
+    int longest=1;
+    for(int i=0;i<n;i++){
+        st.insert(arr[i]);
+    }
+    for(auto it:st){
+        if(st.find(it-1)==st.end()){
+            int count=1;
+            int x=it;
+            while(st.find(x+1)!=st.end()){
+                count++;
+                x++;
+            }
+            longest=max(longest,count);
+        }
+    }
+    return longest;
+}
 //This better solution will have O(nlogn) time complexity and O(1) space complexity
 int longestSubsequenceBetterSolution(vector<int>& arr){
     int n=arr.size();
