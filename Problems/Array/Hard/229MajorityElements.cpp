81fccf6b3fd0d90b26259a200bfc4ac54e241765
mrakib007 <mrakibhasan199744@gmail.com>
2025-01-15 20:47:45 +0600

    optimal solution for majority elements

--- Problems/Array/Hard/229MajorityElements.cpp
+++ Problems/Array/Hard/229MajorityElements.cpp
@@ -1,6 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-//Better approach. Time complexity is O(n) and space complexity is O(1)
+//Optimal approach. Time complexity is O(2n) and space complexity is O(1)
+vector<int> majorityElementOptimal(vector<int>& nums) {
+       int el1=INT_MIN;
+       int el2=INT_MIN;
+       int n=nums.size();
+       int minimum=n/3+1;
+       vector<int>answer;
+       int count1=0,count2=0;
+       for(int i=0;i<n;i++){
+        if(count1==0 && el2!=nums[i]){
+            count1=1;
+            el1=nums[i];
+        }else if(count2==0 && el1!=nums[i]){
+            count2=1;
+            el2=nums[i];
+        }else if(el1==nums[i]) count1++;
+        else if(el2==nums[i]) count2++;
+        else{
+            count1--;
+            count2--;
+            }
+       }
+       count1=0,count2=0;
+       for(int i=0;i<nums.size();i++){
+        if(el1==nums[i]) count1++;
+        if(el2==nums[i]) count2++;
+       }
+       if(count1>=minimum) answer.push_back(el1);
+       if(count2>=minimum) answer.push_back(el2);
+       sort(answer.begin(),answer.end());
+       return answer;
+    }
+//Better approach. Time complexity is O(n) and space complexity is O(n)
 list<int>majorityElementBetter(vector<int>&nums){
     list<int>answer;
     map<int,int>mpp;
@@ -10,6 +42,9 @@ list<int>majorityElementBetter(vector<int>&nums){
         mpp[nums[i]]++;
         if(mpp[nums[i]]==minimumCount){
             answer.push_back(nums[i]);
+        }
+        if(answer.size()==2){
+            break;
         }
     }
     return answer;
