85143f2224d9e3fa0c51133a75b0fc05e9edccc6
mrakib007 <mrakibhasan199744@gmail.com>
2025-01-17 17:26:25 +0600

    Triplet sum optimal approach

--- Problems/Array/Hard/15ThreeSum.cpp
+++ Problems/Array/Hard/15ThreeSum.cpp
@@ -1,5 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimal Approach. Time complexity is O(n^2)+O(nlogn) and space complexity is O(1)
+ vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
 //Better Approach. Time complexity is O(n^2) and space complexity is O(n)
 vector<vector<int>> findTripletBetter(vector<int>& nums) {
         set<vector<int>>st;
