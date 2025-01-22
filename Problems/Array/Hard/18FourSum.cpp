0ef73ea224a8b2e563a551c1fc11b40702a2211d
mrakib007 <mrakibhasan199744@gmail.com>
2025-01-22 21:45:29 +0600

    4 sum optimized

--- Problems/Array/Hard/18FourSum.cpp
+++ Problems/Array/Hard/18FourSum.cpp
@@ -1,5 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized Approach. Time complexity is O(n^3) and space complexity is O(number of quadruplets). It's not used for 
+//solving the problem, rather returning the answer. 
+vector<vector<int>> fourSumOptimized(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>answer;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j-1]==nums[j]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum=sum+nums[j];
+                    sum=sum+nums[k];
+                    sum=sum+nums[l];
+                    if(sum==target){
+                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                        answer.push_back(temp);
+                        k++; l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }else if(sum>target) l--;
+                    else k++;
+                }
+            }
+        }
+        return answer;
+    }
 //Better Approach. Time complexity is O(n^3) and space complexity is O(N) + O(number of quadruplets).
 //O(N) is the space complexity of the inner hashSet and O(number of quadruplets) is the space complexity of the set.
  vector<vector<int>> fourSumBetter(vector<int>& nums, int target) {
