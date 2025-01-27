0935c5a2e68c21400167b9a2a320ce959ba9eb5d
mrakib007 <mrakibhasan199744@gmail.com>
2025-01-27 13:21:08 +0600

    Optimized approach for subarrays with xork k

--- Problems/Array/Hard/subarraysWithXOR.cpp
+++ Problems/Array/Hard/subarraysWithXOR.cpp
@@ -1,5 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+//The optimal approach is O(n) and space complexity is O(n)
+int subArrayWithXOROptimized(vector<int>&arr,int k){
+    int xr=0;
+    map<int,int>mpp;
+    mpp[xr]++;
+    int count=0;
+    for(int i=0;i<arr.size();i++){
+        xr=xr^arr[i];
+        int x=xr^k;
+        count=count+mpp[x];
+        mpp[xr]++;
+    }
+    return count;
+}
+//The optimal approach is O(n^2) and space complexity is O(1)
+int subArrayWithXORBetter(vector<int>&arr,int k){
+    int n=arr.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+        int XOR=0;
+        for(int j=i;j<n;j++){
+            XOR=XOR^arr[j];
+            if(XOR==k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+//The brute force approach is O(n^3) and space complexity is O(1)
 int subArrayWithXORBrute(vector<int>&arr,int k){
     int n=arr.size();
     int count=0;
