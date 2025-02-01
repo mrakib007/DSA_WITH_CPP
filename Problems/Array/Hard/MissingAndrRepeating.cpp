66657e9f06fc8899f1a5bfd709a60a63e6527160
mrakib007 <mrakibhasan199744@gmail.com>
2025-02-01 14:15:43 +0600

    Missing and Repeating element optimized solution

--- Problems/Array/Hard/MissingAndrRepeating.cpp
+++ Problems/Array/Hard/MissingAndrRepeating.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized approach. Time complexity is O(n) and space complexity is O(1)
+vector<int> missingAndRepeatingOptimized(vector<int>& arr) {
+        long long n=arr.size();
+        long long SN=(n*(n+1))/2;
+        long long S2N=(n*(n+1)*(2*n+1))/6;
+        long long S1=0,S2=0;
+        for(int i=0;i<n;i++){
+            S1=S1+arr[i];
+            S2=S2+(long long)arr[i]*(long long)arr[i];
+        }
+        long long val1=S1-SN;
+        long long val2=S2-S2N;
+        val2=val2/val1;
+        long long x=(val1+val2)/2;
+        long long y=x-val1;
+        return {(int)x,(int)y};
+    }
 //Better approach. Time complexity is O(2n) and space complexity is O(n)
 vector<int> missingAndRepeatingBetter(vector<int>& arr) {
         int n=arr.size();
