57cfc4c27d5cd9f2fdb58ca84f9ffdf3acaac7f9
mrakib007 <mrakibhasan199744@gmail.com>
2025-02-01 12:18:43 +0600

    Missing and repeating element, Better approach solution

--- Problems/Array/Hard/MissingAndrRepeating.cpp
+++ Problems/Array/Hard/MissingAndrRepeating.cpp
@@ -1,5 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Better approach. Time complexity is O(2n) and space complexity is O(n)
+vector<int> missingAndRepeatingBetter(vector<int>& arr) {
+        int n=arr.size();
+        int hashArray[n+1]={0};
+        int repeating=-1,missing=-1;
+        for(int i=0;i<n;i++){
+            hashArray[arr[i]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(hashArray[i]>1){
+                repeating=i;
+            }else if(hashArray[i]==0){
+                missing=i;
+            }
+            if(missing!=-1&&repeating!=-1) break;
+        }
+        return {repeating,missing};
+    }
 //brute force approach. Time complexity is O(n^2) and space complexity is O(1)
 vector<int> missingAndRepeatingBrute(vector<int>&nums,int n){
     int repeating=-1,missing=-1;
