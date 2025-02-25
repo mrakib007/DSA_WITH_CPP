ab65e2971ab00ed35792dbfb3819e2f46eebc2fe
mrakib007 <mrakibhasan199744@gmail.com>
2025-02-25 21:26:48 +0600

    Search in rotated array ||

---
+++ Problems/BinarySearch/Easy/searchInRotatedArray2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Time complexity=O(logn), Space complexity=O(1)
+bool searchInRotatedArray(vector<int> arr,int n,int target){
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==target) return true;
+        if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<=target && target<=arr[mid]) high=mid-1;
+            else low=mid+1;
+        }else{
+            if(arr[mid]<=target && arr[high]>=target) low=mid+1;
+            else high=mid-1;
+        }
+    }
+    return false;
+}
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    bool answer=searchInRotatedArray(arr,n,x);
+    cout<<answer<<endl;
+    return 0;
+}
