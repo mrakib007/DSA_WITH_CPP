535325edbcc1926fdfd3605c227c88cc177b8946
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-01 17:52:32 +0600

    Optimized solution for searching single element in the array

--- Problems/BinarySearch/Easy/singleElementInSortedArray.cpp
+++ Problems/BinarySearch/Easy/singleElementInSortedArray.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized Solution
+//Time complexity=O(logn), Space complexity=O(1)
+int findSingleElementOptimized(vector<int>arr,int n){
+    if(n==1) return arr[0];
+    if(arr[n-1]!=arr[n-2]) return arr[n-1];
+    int low=1,high=n-2;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]) return arr[mid];
+        if(mid%2==1 && arr[mid]==arr[mid-1] || mid%2==0 && arr[mid]==arr[mid+1]){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return -1;
+}
 //Brute Force Solution
 //Time complexity=O(n), Space complexity=O(1)
 int findSingleElementBrute(vector<int>arr,int n){
