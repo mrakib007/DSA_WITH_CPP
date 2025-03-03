81ca0c732203e03d789dd8a40f6f69c6dabef9bb
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-03 22:15:03 +0600

    Find square root using binary search(optimized approach)

--- Problems/BinarySearch/Binary Search On Answers/Hard/findSquareRoot.cpp
+++ Problems/BinarySearch/Binary Search On Answers/Hard/findSquareRoot.cpp
@@ -1,5 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized approach using binary search.
+//time complexity is O(logn)
+//space complexity is O(1)
+int findSquareRootOptimized(int n){
+    int low=1,high=n,ans=1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(mid*mid<=n){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return high;
+}
+//Brute force approach.
+//time complexity is O(n)
+//space complexity is O(1)
 int findSquareRootBrute(int n){
     int ans=1;
     for(int i=1;i<=n;i++){
@@ -12,7 +31,7 @@ int findSquareRootBrute(int n){
 int main(){
     int n;
     cin>>n;
-    int ans=findSquareRootBrute(n);
+    int ans=findSquareRootOptimized(n);
     cout<<ans<<endl;
     return 0;
 }
