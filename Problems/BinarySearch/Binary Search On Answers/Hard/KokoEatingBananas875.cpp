09bd0c549258f7fb6d983227c27b32b7ae14992b
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-05 20:04:51 +0600

    Optimized approach with binary search

--- Problems/BinarySearch/Binary Search On Answers/Hard/KokoEatingBananas875.cpp
+++ Problems/BinarySearch/Binary Search On Answers/Hard/KokoEatingBananas875.cpp
@@ -1,5 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized approach with Binary search. 
+//Time complexity is O(nlog(max(arr))*n), Space complexity is O(1)
+long long findHours(vector<int>piles,int mid){
+    long long totalHours=0;
+    int n=piles.size();
+    for(int i=0;i<n;i++){
+        totalHours=totalHours+ceil((double)piles[i]/(double)mid);
+    }
+    return totalHours;
+}
+int findMax(vector<int>piles){
+    int n=piles.size();
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(piles[i]>max){
+            max=piles[i];
+        }
+    }
+    return max;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1,high=findMax(piles);
+        while(low<=high){
+            int mid=(low+high)/2;
+            long long totalHours=findHours(piles,mid);
+            if(totalHours<=h){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
 //Brute force approach
 //Time complexity is O(max(arr)*n), Space complexity is O(1)
 int calculateTime(vector<int> arr,int hourly){
