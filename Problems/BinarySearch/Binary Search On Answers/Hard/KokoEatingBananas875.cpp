756c2829b85c083175eb83b0a2dcb1a2ffde3f80
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-05 16:41:22 +0600

    Koko Eating bananas brute force solution

---
+++ Problems/BinarySearch/Binary Search On Answers/Hard/KokoEatingBananas875.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int calculateTime(vector<int> arr,int hourly){
+    int totalHours=0;
+    for(int i=0;i<arr.size();i++){
+        totalHours+=ceil(arr[i]/hourly);
+    }
+    return totalHours;
+}
+int findHoursBrute(vector<int> arr,int n,int h){
+    int maxVal=*max_element(arr.begin(), arr.end());
+    for(int i=1;i<=maxVal;i++){
+        int requiredTime=calculateTime(arr,i);
+        if(requiredTime<=h) return i;
+    }
+}
+int main(){
+    int n,h;
+    cin>>n>>h;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int ans=findHoursBrute(arr,n,h);
+    cout<<ans<<endl;
+    return 0;
+}
