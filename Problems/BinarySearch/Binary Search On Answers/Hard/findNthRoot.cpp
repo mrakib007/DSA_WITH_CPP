ad128c1bbfa5ab2bada5a6d371bf061d09c5bbfe
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-05 11:38:17 +0600

    Find Nth root optimized

--- Problems/BinarySearch/Binary Search On Answers/Hard/findNthRoot.cpp
+++ Problems/BinarySearch/Binary Search On Answers/Hard/findNthRoot.cpp
@@ -1,5 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Optimized approach
+//time complexity is O(logn)
+//space complexity is O(1)
+int findExp(int x,int n,int m){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans=ans*x;
+        if(ans>m){
+            return 2;
+        }
+    }
+    if(ans==m) return 1;
+    return 0;
+}
+int findNthRootOptimized(int n,int m){
+    int low=1,high=m;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(findExp(mid,n,m)==1){
+            return mid;
+        }else if(findExp(mid,n,m)==0){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return -1;
+}
 //Brute force approach.
 //time complexity is O(n)
 //space complexity is O(1)
