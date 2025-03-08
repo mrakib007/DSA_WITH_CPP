5a3acaa7d1a0d0f38a4b5db5621ec94311da6aca
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-08 16:33:07 +0600

    Optimized solution for finding smallest divisor

--- Problems/BinarySearch/Binary Search On Answers/Hard/Easy/findSmallestDivisor.cpp
+++ Problems/BinarySearch/Binary Search On Answers/Hard/Easy/findSmallestDivisor.cpp
@@ -7,13 +7,28 @@ int findMaxElement(vector<int> arr,int n){
     }
     return max;
 }
-int findSmallestDivisor(vector<int> arr,int n,int threshold){
+int findDivisor(vector<int> arr,int n,int d){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+(ceil((double)arr[i]/d));
+    }
+    return sum;
+}
+//Optimized approach with binary search
+//Time complexity is O(nlog(max(arr))), Space complexity is O(1)
+int findSmallestDivisorOptimized(vector<int>arr,int n, int threshold){
+    if(n>threshold) return -1;
+    int low=1,high=findMaxElement(arr,n);
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(findDivisor(arr,n,mid)<=threshold) high=mid-1;
+        else low=mid+1;
+    }
+    return low;
+}
+int findSmallestDivisorBrute(vector<int> arr,int n,int threshold){
     for(int d=1;d<=findMaxElement(arr,n);d++){
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum=sum+(ceil((double)arr[i]/d));
-        }
-        if(sum<=threshold) return d;
+        if(findDivisor(arr,n,d)<=threshold) return d;
     }
     return -1;
 }
@@ -22,7 +37,7 @@ int main(){
     cin>>n>>threshold;
     vector<int> arr(n);
     for(int i=0;i<n;i++) cin>>arr[i];
-    int ans=findSmallestDivisor(arr,n,threshold);
+    int ans=findSmallestDivisorBrute(arr,n,threshold);
     cout<<ans<<endl;
     return 0;
 }
