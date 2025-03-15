8f12137000bd53a39cfc279eacad5865215937b0
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-15 16:57:48 +0600

    Optimiezed solution using binary search

--- Problems/BinarySearch/minOfMaxOrMaxOfMin/minDistanceOfCows.cpp
+++ Problems/BinarySearch/minOfMaxOrMaxOfMin/minDistanceOfCows.cpp
@@ -10,6 +10,29 @@ bool canPlace(vector<int>&stalls,int n,int dist,int cows){
     }
     return countCows>=cows;
 }
+//Optimized approach using binary search
+//Time complexity : O(nlogn + log(max-min) + n), space complexity : O(1)
+int aggressiveCows(vector<int> &stalls, int c)
+{
+    int ans=-1;
+    int maximum=*max_element(stalls.begin(),stalls.end());
+    int minimum=*min_element(stalls.begin(),stalls.end());
+    int n=stalls.size();
+    sort(stalls.begin(),stalls.end());
+    int low=0,high=maximum-minimum;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(canPlace(stalls,n,mid,c)){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return high;
+}
+//Brute force approach 
+//Time complexity : O(max-min)*O(n), space complexity : O(1)
 int findMinDistanceBrute(vector<int>&stalls,int n,int c){
     int maximum=*max_element(stalls.begin(),stalls.end());
     int minimum=*min_element(stalls.begin(),stalls.end());
