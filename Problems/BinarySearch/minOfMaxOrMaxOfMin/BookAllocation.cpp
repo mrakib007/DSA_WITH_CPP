3eddfa1593c98c7699bf9ccd7eeafae16bd20c39
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-16 23:08:49 +0600

    Allocate books brute force solution

---
+++ Problems/BinarySearch/minOfMaxOrMaxOfMin/BookAllocation.cpp
@@ -0,0 +1,37 @@
+//Hard Problem
+#include<bits/stdc++.h>
+using namespace std;
+int maxPages(vector<int> pages,int n,int pageNum){
+    int student=1,pagesForStudent=0;
+    for(int i=0;i<n;i++){
+        if(pagesForStudent+pages[i]<=pageNum){
+            pagesForStudent+=pages[i];
+        }else{
+            student++;
+            pagesForStudent=pages[i];
+        }
+    }
+    return student;
+}
+//Brute force solution
+//Time complexity : O(sum-max+1)*n
+//Space complexity : O(1)
+int findPagesBrute(vector<int> pages,int n,int k){
+    if(n<k) return -1;
+    int low=*max_element(pages.begin(),pages.end());
+    int high=accumulate(pages.begin(),pages.end(),0);
+    for(int pageNum=low;pageNum<=high;pageNum++){
+        int countStudent=maxPages(pages,n,pageNum);
+        if(countStudent==k) return pageNum;
+    }
+    return -1;
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> pages(n);
+    for(int i=0;i<n;i++) cin>>pages[i];
+    int ans=findPagesBrute(pages,n,k);
+    cout<<ans<<endl;
+    return 0;
+}
