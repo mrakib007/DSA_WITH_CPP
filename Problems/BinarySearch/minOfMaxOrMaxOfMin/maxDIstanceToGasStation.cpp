2d899dc9709337be088174e2ad6b915b0fa63faa
mrakib007 <mrakibhasan199744@gmail.com>
2025-03-21 16:55:54 +0600

    max distance to gas station

---
+++ Problems/BinarySearch/minOfMaxOrMaxOfMin/maxDIstanceToGasStation.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxDistanceBrute(vector<int>arr,int n,int k){
+    int n=arr.size();
+    vector<int>howMany(n-1,0);
+    for(int gasStations=1;gasStations<=k;gasStations++){
+        int maxSection=-1;
+        int maxIndex=-1;
+        for(int i=0;i<n-1;i++){
+            long double diff=(arr[i+1]-arr[i]);
+            long double sectionLength=diff/(long double)(howMany[i]+1);
+            if(sectionLength>maxSection){
+                maxSection=sectionLength;
+                maxIndex=i;
+            }
+        }
+        howMany[maxIndex]++;
+    }
+    long double maxAns=-1;
+    for(int i=0;i<n-1;i++){
+        long double diff=(arr[i+1]-arr[i]);
+        long double sectionLength=diff/(long double)(howMany[i]+1);
+        maxAns=max(maxAns,sectionLength);
+    }
+    return maxAns;
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int ans=maxDistanceBrute(arr,n,k);
+    cout<<ans<<endl;
+    return 0;
+}
